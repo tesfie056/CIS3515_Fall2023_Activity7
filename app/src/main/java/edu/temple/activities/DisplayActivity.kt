b14bc75e4c7861017b2b86a4fa934ebbb895076e
tesfie056 <143583774+tesfie056@users.noreply.github.com>
2023-10-05 15:18:10 -0400

    launched TextSizeActivity by creating SetOnclinckListener

--- app/src/main/java/edu/temple/activities/DisplayActivity.kt
+++ app/src/main/java/edu/temple/activities/DisplayActivity.kt
@@ -1,10 +1,15 @@
 package edu.temple.activities
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.TypedValue
 import android.widget.Button
 import android.widget.TextView
+import kotlinx.coroutines.flow.DEFAULT_CONCURRENCY_PROPERTY_NAME
 
+const val REQUEST_NAME = "Message"
+const val DEFAULT_TEXT_SIZE = 1
 class DisplayActivity : AppCompatActivity() {
 
     // TODO Step 1: Launch TextSizeActivity when button clicked to allow selection of text size value
@@ -21,5 +26,20 @@ class DisplayActivity : AppCompatActivity() {
         lyricsDisplayTextView = findViewById(R.id.lyricsDisplayTextView)
         textSizeSelectorButton = findViewById(R.id.textSizeSelectorButton)
 
+        textSizeSelectorButton.setOnClickListener {TextSize ->
+            val launtchIntent = Intent(this@DisplayActivity, TextSizeActivity::class.java)
+           launtchIntent.putExtra(REQUEST_NAME, lyricsDisplayTextView.textSize )
+            startActivity(launtchIntent)
+
+        }
+        val textSize = intent.getFloatExtra(REQUEST_NAME, DEFAULT_TEXT_SIZE.toFloat())
+        lyricsDisplayTextView.textSize = textSize
     }
+        val message = intent.getStringExtra("message")
+//with(findViewById<TextView>(R.id.textSizeSelectorButton)){
+//    val transfer = intent.getIntExtra(REQUEST_NAME, DEFAULT_TEXT_SIZE)
+//    setTextSize(TypedValue.COMPLEX_UNIT_PX,transfer.toFloat())
+//}
+//    }
+
 }
--- app/src/main/java/edu/temple/activities/TextSizeActivity.kt
+++ app/src/main/java/edu/temple/activities/TextSizeActivity.kt
@@ -1,5 +1,7 @@
 package edu.temple.activities
 
+import android.app.Activity
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
@@ -23,8 +25,12 @@ class TextSizeActivity : AppCompatActivity() {
         with (findViewById(R.id.textSizeSelectorRecyclerView) as RecyclerView) {
 
             // TODO Step 2: Pass selected value back to activity that launched TextSizeActivity
-            adapter = TextSizeAdapter(textSizes){
 
+            adapter = TextSizeAdapter(textSizes){selectedTextSize ->
+                val result = Intent()
+                result.putExtra("textSize", selectedTextSize)
+                setResult(Activity.RESULT_OK, result)
+                finish()
             }
             layoutManager = LinearLayoutManager(this@TextSizeActivity)
         }
